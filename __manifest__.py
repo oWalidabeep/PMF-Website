dbaa65ced40cd0b9bfebfe1983f5589b9a9653cf
oWalidabeep <walid.babo@ac-versailles.fr>
2025-04-14 21:51:19 +0200

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "PMF Website Theme",
+    "version": "1.0",
+    "category": "Theme",
+    "summary": "Thème personnalisé pour le lycée Pierre Mendès France",
+    "author": "ChatGPT",
+    "website": "https://www.odoo.com",
+    "depends": ["website", "website_blog", "website_sale", "event"],
+    "data": [
+        "views/assets.xml",
+        "views/layout.xml"
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False
+}
